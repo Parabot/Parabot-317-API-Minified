18e5b165084ca243266506e7c04fd818b2ecb532
Clisprail <clisprail@parabot.org>
2014-03-13 00:24:39 +0100

    Loader

---
+++ src/org/rev317/Loader.java
@@ -0,0 +1,42 @@
+package org.rev317;
+
+import java.applet.Applet;
+import java.io.File;
+import java.net.URL;
+
+import org.parabot.core.Context;
+import org.parabot.core.asm.ASMClassLoader;
+import org.parabot.environment.servers.ServerManifest;
+import org.parabot.environment.servers.ServerProvider;
+import org.parabot.environment.servers.Type;
+
+@ServerManifest(author = "Everel", name = "Local Client", type = Type.INJECTION, version = 0.1)
+public class Loader extends ServerProvider {
+	private Applet applet;
+
+	@Override
+	public Applet fetchApplet() {
+		try {
+			final Context context = Context.getInstance();
+			final ASMClassLoader classLoader = context.getASMClassLoader();
+			final Class<?> clientClass = classLoader.loadClass("client");
+			Object instance = clientClass.newInstance();
+			applet = (Applet) instance;
+			return applet;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public URL getJar() {
+		try {
+			return new File("D:/317client.jar").toURI().toURL();
+		} catch(Throwable t) {
+			t.printStackTrace();
+		}
+		return null;
+	}
+
+}
