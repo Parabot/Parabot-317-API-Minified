ee2656cdd063bfd40d7fe8738ed66118b4f29d36
matt123337 <matt123337@users.noreply.github.com>
2014-07-16 14:22:07 -0400

    Update ActionListener.java
    Fixed another silly mistake on my part, really shouldn't have been doing these edits on github itself.

--- src/org/rev317/min/api/events/listeners/ActionListener.java
+++ src/org/rev317/min/api/events/listeners/ActionListener.java
@@ -1,7 +1,9 @@
 package org.rev317.min.api.events.listeners;
 
+import org.rev317.min.api.events.ActionEvent;
+
 public interface ActionListener{
 
-    public void onGameAction(int id,int cmd1,int cmd2,int cmd3);
+    public void onGameAction(ActionEvent event);
 
 }
