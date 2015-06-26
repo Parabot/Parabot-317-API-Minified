fdbc8433ea5682ecf5cac2bc549b725f00de1c51
JKetelaar <jeroen@ketelaar.me>
2015-06-26 13:07:18 +0200

    Fixed name issue in Prayer

--- src/org/rev317/min/api/methods/Prayer.java
+++ src/org/rev317/min/api/methods/Prayer.java
@@ -5,49 +5,47 @@ import org.parabot.environment.scripts.framework.SleepCondition;
 
 /**
  * @author JKetelaar, Fryslan
- * TODO Set the actual names and level requirements
+ * TODO Set the actual level requirements
  */
 public enum Prayer {
-    THICK_SKIN(83, 5609, "name", 1),
-    BURST_OF_STRENGTH(84, 5610, "name", 1),
-    CLARITY_OF_THOUGHT(85, 5611, "name", 1),
-    SHARP_EYE(700, 19812, "name", 1),
-    MYSTIC_WILL(701, 19814, "name", 1),
-    ROCK_SKIN(86, 5612, "name", 1),
-    SUPERHUMAN_STRENGTH(87, 5613, "name", 1),
-    IMPROVED_REFLEXES(88, 5614, "name", 1),
-    RAPID_RESTORE(89, 5615, "name", 1),
-    RAPID_HEAL(90, 5616, "name", 1),
-    PROTECT_ITEM(91, 5617, "name", 1),
-    HAWK_EYE(702, 19816, "name", 1),
-    MYSTIC_LORE(703, 19818, "name", 1),
-    STEEL_SKIN(92, 5618, "name", 1),
-    ULTIMATE_STRENGTH(93, 5619, "name", 1),
-    INCREDIBLE_REFLEXES(94, 5620, "name", 1),
-    PROTECT_FROM_SUMMONING(708, 23105, "name", 1),
-    PROTECT_FROM_MAGIC(95, 5621, "name", 1),
-    PROTECT_FROM_MISSILES(96, 5622, "name", 1),
-    PROTECT_FROM_MELEE(97, 5623, "name", 1),
-    EAGLE_EYE(704, 19823, "name", 1),
-    MYSTIC_MIGHT(705, 19823, "name", 1),
-    RETRIBUTION(98, 683, "name", 1),
-    REDEMPTION(99, 684, "name", 1),
-    SMITE(100, 685, "name", 1),
-    CHIVALRY(706, 19825, "name", 1),
-    RAPID_RENEWAL(709, 23109, "name", 1),
-    PIETY(707, 19827, "name", 1),
-    RIGOUR(710, 23113, "name", 1),
-    AUGURY(711, 23116, "name", 1);
+    THICK_SKIN(83, 5609, 1),
+    BURST_OF_STRENGTH(84, 5610, 1),
+    CLARITY_OF_THOUGHT(85, 5611, 1),
+    SHARP_EYE(700, 19812, 1),
+    MYSTIC_WILL(701, 19814, 1),
+    ROCK_SKIN(86, 5612, 1),
+    SUPERHUMAN_STRENGTH(87, 5613, 1),
+    IMPROVED_REFLEXES(88, 5614, 1),
+    RAPID_RESTORE(89, 5615, 1),
+    RAPID_HEAL(90, 5616, 1),
+    PROTECT_ITEM(91, 5617, 1),
+    HAWK_EYE(702, 19816, 1),
+    MYSTIC_LORE(703, 19818, 1),
+    STEEL_SKIN(92, 5618, 1),
+    ULTIMATE_STRENGTH(93, 5619, 1),
+    INCREDIBLE_REFLEXES(94, 5620, 1),
+    PROTECT_FROM_SUMMONING(708, 23105, 1),
+    PROTECT_FROM_MAGIC(95, 5621, 1),
+    PROTECT_FROM_MISSILES(96, 5622, 1),
+    PROTECT_FROM_MELEE(97, 5623, 1),
+    EAGLE_EYE(704, 19823, 1),
+    MYSTIC_MIGHT(705, 19823, 1),
+    RETRIBUTION(98, 683, 1),
+    REDEMPTION(99, 684, 1),
+    SMITE(100, 685, 1),
+    CHIVALRY(706, 19825, 1),
+    RAPID_RENEWAL(709, 23109, 1),
+    PIETY(707, 19827, 1),
+    RIGOUR(710, 23113, 1),
+    AUGURY(711, 23116, 1);
 
     private int setting;
     private int action;
-    private String name;
     private int level;
 
-    Prayer(int setting, int action, String name, int level) {
+    Prayer(int setting, int action, int level) {
         this.setting = setting;
         this.action = action;
-        this.name = name;
         this.level = level;
     }
 
