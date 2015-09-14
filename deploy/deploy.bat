c96ad6be51a21692397e4951f0409a1dd6bd30ff
JKetelaar <jeroen@ketelaar.me>
2015-09-14 13:49:38 +0200

    Updated POM

---
+++ deploy/deploy.bat
@@ -0,0 +1,3 @@
+mvn install:install-file -DgroupId=${project.groupId} -DartifactId=${project.artifactId} -Dversion=${project.version} -Dpackaging=jar -Dfile=../target/${project.build.finalName}.jar -DlocalRepositoryPath=../../Maven-Repository
+
+:: mvn install:install-file -DgroupId=org.parabot -DartifactId=317-api-minified -Dversion=1.12 -Dpackaging=jar -Dfile=../target/317-api-minified-V1.12.jar -DlocalRepositoryPath=../../Maven-Repository
