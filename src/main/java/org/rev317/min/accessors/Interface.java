e508d25d1fb83afcbee50f0d76e444c72de1c20a
JKetelaar <jeroen@ketelaar.me>
2015-12-30 18:11:37 +0100

    [BUGFIX] Added Interface#getLongStackSizes

--- src/main/java/org/rev317/min/accessors/Interface.java
+++ src/main/java/org/rev317/min/accessors/Interface.java
@@ -6,6 +6,13 @@ public interface Interface {
 
     public int[] getStackSizes();
 
+    /**
+     * This is meant for the clients that have a long value as their stacksizes, simply because they have no logic...
+     *
+     * @return long version of #getStackSizes
+     */
+    public long[] getLongStackSizes();
+
     public String getMessage();
 
 }
